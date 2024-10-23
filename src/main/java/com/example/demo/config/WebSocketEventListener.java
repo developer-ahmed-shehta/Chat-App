c37910dc035b44f649fdcf274ecc49969a00fe5e
developer-ahmed-shehta <ahmedshehta0123@gmail.com>
2024-10-23 05:33:12 +0300

    finish backend start frontend

--- src/main/java/com/example/demo/config/WebSocketEventListener.java
+++ src/main/java/com/example/demo/config/WebSocketEventListener.java
@@ -1,5 +1,23 @@
 package com.example.demo.config;
 
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class WebSocketEventListener {
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
+        // TO DO // Inform the user of chat application that user or one user are left
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+    }
 
 }
